72ec1ca46d106f69092605a7a7f6c95cffd4eaf3
idcs-source <chub_idcs@proton.me>
2026-07-31 09:44:38 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+Hooks.once("init", () => {
+  CONFIG.DND5E.tools.personamic = {
+    ability: "cha",
+    id: "Item.eeLb1jg2RfHlDvep"
+  };
+  console.log("Persona Mic Tool | Registered personamic in CONFIG.DND5E.tools");
+});
