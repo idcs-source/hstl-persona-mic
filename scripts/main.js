df114a13085b3d9af5a0d7ae04f26216451dd102
idcs-source <chub_idcs@proton.me>
2026-07-31 09:56:24 -0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 Hooks.once("init", () => {
   CONFIG.DND5E.tools.personamic = {
     ability: "cha",
-    id: "Item.eeLb1jg2RfHlDvep"
+    id: "Compendium.world.hstl-homebrew.Item.eeLb1jg2RfHlDvep"
   };
   console.log("Persona Mic Tool | Registered personamic in CONFIG.DND5E.tools");
 });
